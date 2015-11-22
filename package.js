443a76d097f386a62a6134359e4d117918bc2f94
sunhannan <cto@fami2u.com>
2015-11-22 20:42:57 +0800

    fix bug

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'fami:account',
-    version: '0.0.6',
+    version: '0.0.7',
     summary: 'fami base account',
     git: 'https://github.com/fami2u/meteor-fami-account',
     documentation: 'README.md'
